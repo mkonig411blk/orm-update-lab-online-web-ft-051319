98a78bcc9fd068de2d430767009705c756f24c2e
Margot Konig <margot.konig@blackrock.com>
2019-06-12 23:44:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,7 @@ def save
     self.update
   else
     sql = <<-SQL
-      INSERT INTO student (name, grade)
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
