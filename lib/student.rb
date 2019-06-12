f187f1b8022d3bece3acc7435cc4d09d6a446491
Margot Konig <margot.konig@blackrock.com>
2019-06-12 23:30:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,50 @@
 require_relative "../config/environment.rb"
-
 class Student
-  attr_accessible :name, :grade
+  attr_accessor :name, :grade
   attr_reader :id
-
+  
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+  
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade INTEGER
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
+  
+  def self.drop_table
+     sql =  <<-SQL 
+      DROP TABLE students
+        SQL
+    DB[:conn].execute(sql) 
+  end
+  
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) 
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.grade)
+ 
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end 
+  
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student 
+  end
+  
+  
  def self.new_from_db(row)
     new_student = self.new
     new_student.id = row[0]
@@ -11,24 +52,7 @@ class Student
     new_student.grade = row[2]
     new_student  # return the newly created instance
   end
-  
- def self.create_table
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      grade TEXT
-    )
-    SQL
 
-    DB[:conn].execute(sql)
-  end
-
-  def self.drop_table
-    sql = "DROP TABLE IF EXISTS students"
-    DB[:conn].execute(sql)
-  end
-  
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
@@ -42,17 +66,4 @@ class Student
     end.first
   end
   
-  def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade) 
-      VALUES (?, ?)
-    SQL
-
-    DB[:conn].execute(sql, self.name, self.grade)
-  end
-
- def self.drop_table
-    sql = "DROP TABLE IF EXISTS students"
-    DB[:conn].execute(sql)
-  end
 end
