0c3118578fad81e74ea7715b98f3df139f7ca481
Margot Konig <margot.konig@blackrock.com>
2019-06-12 23:32:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,4 +68,9 @@ end
     end.first
   end
   
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+  
 end
