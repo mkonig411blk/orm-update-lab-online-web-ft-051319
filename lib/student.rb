a4ec2bdda167efc3f3ea3d99e2a54ef960a59e95
Margot Konig <margot.konig@blackrock.com>
2019-06-12 23:51:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,10 +49,10 @@ end
   
   
  def self.new_from_db(row)
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name =  row[1]
-    new_student.grade = row[2]
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
     new_student  # return the newly created instance
   end
 
